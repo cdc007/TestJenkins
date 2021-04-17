f154a2db9f5ecd1412cf4433a4cf523144cfa147
cdc007 <dacheng_chen@yahoo.com>
2021-04-17 11:48:35 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,8 +19,8 @@ node{
 
         def response =sh (
                 script: '''
-curl --location --request POST 'https://api.beta.shipwire.com/exec/InventoryServices.php' \
---header 'Content-Type: application/xml' \
+curl --location --request POST 'https://api.beta.shipwire.com/exec/InventoryServices.php' 
+--header 'Content-Type: application/xml' 
 --data-raw "${content}"
             ''',
                 returnStdout: true
