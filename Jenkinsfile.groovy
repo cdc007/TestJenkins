165a5959ade1d183bae1693b5d942a0aa1619821
cdc007 <dacheng_chen@yahoo.com>
2021-04-16 22:33:25 -0700

    all

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,59 @@
+@Library ('TestJenkins@master') _
+
+
+
+node{
+
+    stage('prepare'){
+
+        checkout scm
+        println sh(script: 'pwd', returnStdout: true).trim()
+        println env.GITTY
+
+        echo "hhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+        println sh(script: 'ls -lrt', returnStdout: true).trim()
+
+        workspace = env.WORKSPACE
+        echo "Current workspace is ${env.WORKSPACE}"
+        path= "${workspace}/RequestXML/test.xml"
+
+        def data = readFile(file: '/testzorg.txt')
+
+    }
+    stage('Audit tools') {
+
+        sh '''
+                           git version
+                           
+                        '''
+
+
+    }
+    stage('build'){
+
+        // echo "Build version ${version} with stuffix ${VERSION_RC}"
+        //sh '''
+        //                         echo "run test.sh"
+        //
+        //                       ./test.sh
+        //                    '''
+        //          testshare message: 'hello frank'
+        //          println this.getClass()
+
+        //     def response = sh(script: 'curl https://cat-fact.herokuapp.com/facts/', returnStdout: true)
+        //       sh "curl https://cat-fact.herokuapp.com/facts/"
+        //    script {
+        //     final String url = "https://dog.ceo/api/breeds/list/all"
+
+        //     final String response = sh(script: "curl -s $url", returnStdout: true).trim()
+
+        //     echo response
+        //    echo "sssssssssssssssssssssssssssssssssssssss"
+        //          }
+
+
+    }
+
+}
+
+
