1dc18f48b6fee5341dc6c13c31d15928514b7bd2
cdc007 <dacheng_chen@yahoo.com>
2021-04-29 17:46:33 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,12 +5,12 @@
 
 @Library('commonLib@master') _
 
-def map = [name:"Jerry", age: 42, city: "New York", hobby:"Singing"]
+def map1 = [name:"Jerry", age: 42, city: "New York", hobby:"Singing"]
 
 timestamps {
 
        SharedLib(
            url: "https://api.beta.shipwire.com/exec/InventoryServices.php",
-           approvers:"${map}"
+              approvers:${map1}
        )
 }
